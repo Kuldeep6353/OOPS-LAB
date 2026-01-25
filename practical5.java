78583a25adf731b7e07bbf470a1d90f0d0a43312
KULDIP AHIR <djkuldeepahir@gmail.com>
2026-01-25 18:04:21 +0530

    Add files via upload

---
+++ practical5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class ATMSimulator {
+    public static void main(String[] args) {
+
+        System.out.println("NAME:KULDIP AHIR (240390107018)");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter amount: ");
+        int amount = scanner.nextInt();
+        
+        System.out.println("\n100: " + amount / 100);
+        amount = amount % 100;
+        
+        System.out.println("50: " + amount / 50);
+        amount = amount % 50;
+        
+        System.out.println("10: " + amount / 10);
+        amount = amount % 10;
+        
+        System.out.println("5: " + amount / 5);
+        amount = amount % 5;
+        
+        System.out.println("2: " + amount / 2);
+        amount = amount % 2;
+        
+        System.out.println("1: " + amount);
+        
+        scanner.close();
+    }
+}
